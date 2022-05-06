8e29b4f1f01638edf17b3691b35fc7cfa1fa1061
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:41:00 +0530

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,2 +1,24 @@
-provider aws{}
+resource "aws_security_group" "sg" {
+  name = "${var.sg}"
+  vpc_id = "${var.vpc_id}"
+
+}
+
+resource "aws_security_group" "ingress_sg" {
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
+resource "aws_security_group" "egress_sg" {
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
