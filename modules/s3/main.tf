9f05e75952b4af3d42fd957279ddf2e0c19478aa
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:39:54 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,2 +1,45 @@
-provider aws{}
+resource "aws_s3_bucket"  "terraform-v12-project" {
+  bucket = "${var.bucketname}"
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform-v12-project" {
+  bucket = "${var.bucketname}"
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "terraform-v12-project_bucket" {
+  bucket = "${var.bucketname}"
+  acl    = "${var.acl}"
+}
+   
+resource "aws_s3_bucket_versioning" "terraform-v12-project" {
+  bucket = "${var.bucketname}"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "terraform-v12-project" {
+  bucket = "${var.bucketname}"
+  rule {
+    id = "${var.id}"
+    expiration {
+      days = 90
+    }
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+}
+
 
