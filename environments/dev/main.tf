1ee259ecc4c5f2cad140ccd0d18ef1e3e2e2fa23
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:48:52 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "/home/fl_lpt-265/terraform-modules-workspace/modules/s3"
+  source = "~/modules/s3"
   bucketname = "${var.bucketname}"
   acl = "${var.acl}"
   id = "${var.id}"
