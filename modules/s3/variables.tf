83ea9c781e9982caef2592b8363999c6936c42f8
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:40:18 +0530

    Add files via upload

---
+++ modules/s3/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {}
+
+variable "instance_type" {}
+
+variable "subnet" {}
+
+variable "volume" {}
+
+variable "volumesize" {}
+
+variable "name" {}
+
+variable "env" {}
+
+variable "OS" {}
