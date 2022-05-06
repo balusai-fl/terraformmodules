c381d89c18670706f5c5f1e2cb63db06d5d25382
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:46:36 +0530

    Update main.tf

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,2 +1,30 @@
-provider aws{}
+module "vpc" {
+  source = "/home/fl_lpt-265/terraform-modules-workspace/modules/vpc"
+  cidr = "${var.cidr}"
+  cidrpublic = "${var.cidrpublic}"
+  publicsubnetname =  "${var.publicsubnetname}"
+  cidrprivate = "${var.cidrprivate}"
+  privatesubnetname =  "${var.privatesubnetname}"
+  az1 = "${var.az1}"
+  az2 = "${var.az2}"
+}
+
+module "security" {
+  source = "/home/fl_lpt-265/terraform-modules-workspace/modules/security"
+  sg = "${var.sg}"
+  vpc_id = "${module.vpc.cidr[0]}"
+}
+
+module "ec2" {
+  source = "/home/fl_lpt-265/terraform-modules-workspace/modules/ec2"
+  count   = "${var.instance_count}" 
+  ami =  "${var.ami}"
+  instance_type = "${var.instance_type}"
+  subnet = "${module.vpc.public_subnet[count.index]}"
+  volumesize =  "${var.volumesize}"
+  volume = "${var.volume}"
+  name = "${var.name}-${var.instance_count}"
+  env = "${var.env}"
+  OS = "${var.OS}"
+}
 
