afda172d101dfa6333eeebb60a48caff2c630499
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:42:18 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,2 +1,24 @@
-provider aws{}
+resource "aws_vpc" "main" {
+  cidr_block = "${var.cidr}"
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "${var.cidrpublic}"
+  availability_zone = "${var.az1}"
+
+  tags = {
+    Name = "${var.publicsubnetname}"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "${var.cidrprivate}"
+  availability_zone = "${var.az2}"
+
+  tags = {
+    Name = "${var.privatesubnetname}"
+  }
+}
 
