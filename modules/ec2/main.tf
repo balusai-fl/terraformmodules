0ebd22d81a09c94a88910ca525d78664587455a6
Preethi0499 <81558566+Preethi0499@users.noreply.github.com>
2022-05-06 14:39:12 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,2 +1,19 @@
-provider aws{}
+resource "aws_instance" "instance1" {
+  ami =  "${var.ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${var.subnet}"
+  associate_public_ip_address = true
+  
+  root_block_device {
+    delete_on_termination = true
+    volume_size = "${var.volumesize}"
+    volume_type = "${var.volume}"
+  }
+
+  tags = {
+    Name = "${var.name}"
+    Environment = "${var.env}"
+    OS = "${var.OS}"
+  }
+}
 
